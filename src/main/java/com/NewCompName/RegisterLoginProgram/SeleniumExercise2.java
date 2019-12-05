2c8d1d72f208431b9e7d52096dc3723e32392a90
Suryahere06 <58037576+Suryahere06@users.noreply.github.com>
2019-12-04 22:45:26 -0500

    version1

--- src/main/java/com/NewCompName/RegisterLoginProgram/SeleniumExercise2.java
+++ src/main/java/com/NewCompName/RegisterLoginProgram/SeleniumExercise2.java
@@ -38,8 +38,7 @@ private static void getTitle1(String Title,WebDriver D )
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		
-String S = "Test";
-	//Navigating to the website		
+ 	//Navigating to the website		
 	driver.get("http://demo.automationtalks.com/index.html");
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
